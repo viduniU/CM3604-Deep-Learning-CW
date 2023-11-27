dcdcd32bf5babf3e14d751931928a19cac85e8d7
Dulaj Lochitha <119834481+dulajjayakody@users.noreply.github.com>
2023-11-27 17:18:44 +0530

    Update distilbert.py

--- distilbert.py
+++ distilbert.py
@@ -39,4 +39,18 @@ yelp_subset.to_json(output_json_file_path, orient='records', lines=True)
 
 #display the shape of the loaded subset
 print("Shape of Yelp Subset:", yelp_subset.shape)
+#derive sentiment labels from star labels
+yelp_subset['sentiment'] = yelp_subset['stars'].apply(lambda x: 'positive' if x > 3 else ('negative' if x < 3 else 'neutral'))
+#handle imbalane classes with random oversampling
+x_train, x_test, y_train, y_test = train_test_split(yelp_subset['text'], yelp_subset['sentiment'], test_size=0.2, random_state=42)
+#create the RandomUnderSampler
+undersampler = RandomUnderSampler(sampling_strategy='auto', random_state=42)
+
+#resample the training data
+x_train_resampled, y_train_resampled = undersampler.fit_resample(yelp_subset['text'].to_frame(), yelp_subset['sentiment'])
+
+# Convert the resampled data back to Series
+x_train_resampled = x_train_resampled['text']
+y_train_resampled = pd.Series(y_train_resampled)
+
 
